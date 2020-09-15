efac13696c0bc571a15c588233123358d848cf6d
sim-eunji <kingeunji12@gmail.com>
2020-09-15 17:35:39 +0900

    Feature 기본 환경설정 (#1)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './styles/common.scss'
 
 Vue.config.productionTip = false
 
+const EventBus = new Vue
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
+
+Vue.mixin({
+  computed: {
+    $eventBus: () => EventBus
+  }
+})
